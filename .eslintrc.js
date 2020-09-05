704443eaf39907cf5f12ba7a53c24e06326067df
vladhuk <vladhookovskiy@gmail.com>
2020-09-05 16:00:29 +0300

    Set up project

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended', // Disable base rules
+    'plugin:@typescript-eslint/recommended', // Enable ts rules
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['prettier', '@typescript-eslint'],
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+    },
+  },
+
+  ignorePatterns: ['**/*.css', '**/*.scss', '**/*.svg'],
+
+  rules: {
+    'prettier/prettier': [WARN],
+
+    'no-use-before-define': WARN,
+    'no-unused-expressions': OFF,
+
+    '@typescript-eslint/no-unused-expressions': 'error',
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/order': WARN,
+    'import/prefer-default-export': WARN,
+    'import/no-unresolved': OFF, // Using typescript resolver instead
+
+    'react/jsx-filename-extension': OFF,
+    'react/prop-types': OFF,
+  },
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+  return <div>Hello!</div>;
+};
+
+export default App;
