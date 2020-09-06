0fc33f1dafa79f29d9bb93f6a7e4d3a6adf97bd6
vladhuk <vladhookovskiy@gmail.com>
2020-09-06 13:57:33 +0300

    - Add style; - Add image;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,18 +3,22 @@ import { Stage, Layer } from 'react-konva';
 import Grid from '../Grid';
 import './App.css';
 import Axes from '../Axes';
+import exampleImage from '../../assets/examples/example1.png';
 
 const App: FunctionComponent = () => {
   const width = 700;
   const height = 700;
 
   return (
-    <Stage width={width} height={height}>
-      <Layer>
-        <Grid width={width} height={height} spacing={25} />
-        <Axes width={width} height={height} />
-      </Layer>
-    </Stage>
+    <div className="d-flex">
+      <Stage width={width} height={height}>
+        <Layer>
+          <Grid width={width} height={height} spacing={25} />
+          <Axes width={width} height={height} />
+        </Layer>
+      </Stage>
+      <img src={exampleImage} alt="example" style={{ height }} />
+    </div>
   );
 };
 
