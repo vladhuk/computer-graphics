aa678dce285e6eb1cb7e0438227746a020f7c38b
vladhuk <vladhookovskiy@gmail.com>
2020-09-22 18:40:29 +0300

    - Add graphic functions;

---
+++ src/util/grapchicFunctions.ts
@@ -0,0 +1,17 @@
+import Coord from '../types/Coord';
+
+export function degreesToRad(degrees: number): number {
+  return degrees * (Math.PI / 180);
+}
+
+export function bindRotatePoint(
+  degreess: number,
+  pivot: Coord
+): (point: Coord) => Coord {
+  const rad = degreesToRad(degreess);
+
+  return ({ x, y }) => ({
+    x: (x - pivot.x) * Math.cos(rad) - (y - pivot.y) * Math.sin(rad) + pivot.x,
+    y: (x - pivot.x) * Math.sin(rad) + (y - pivot.y) * Math.cos(rad) + pivot.y,
+  });
+}
