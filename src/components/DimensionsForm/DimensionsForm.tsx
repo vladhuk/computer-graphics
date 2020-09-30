d3a352ce4cdd5465f616f6e6cfd267bf87cc4d81
vladhuk <vladhookovskiy@gmail.com>
2020-09-30 23:04:17 +0300

    - Add optional onSelect tab callback;

--- src/components/DimensionsForm/DimensionsForm.tsx
+++ src/components/DimensionsForm/DimensionsForm.tsx
@@ -10,13 +10,14 @@ export interface FormTab {
 
 interface Props {
   tabs: FormTab[];
+  onSelect?: (tabName: string | null) => void;
 }
 
-const DimensionsForm: FunctionComponent<Props> = ({ tabs }) => {
+const DimensionsForm: FunctionComponent<Props> = ({ tabs, onSelect }) => {
   return (
     <div className="col-3">
       {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-      <Tabs transition={AccordionCollapse as any}>
+      <Tabs transition={AccordionCollapse as any} onSelect={onSelect}>
         {tabs.map(({ title, inputsGroups }) => (
           <Tab key={title} eventKey={title} title={title}>
             <FormTabContent key={title} inputsGroups={inputsGroups} />
