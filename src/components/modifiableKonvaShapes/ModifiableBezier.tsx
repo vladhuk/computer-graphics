dccc72aa4a6bdac7847da7d3d8009d4a963b568f
vladhuk <vladhookovskiy@gmail.com>
2020-10-28 13:35:34 +0200

    - Add modifiable bezier;

--- src/components/modifiableKonvaShapes/ModifiableBezier.tsx
+++ src/components/modifiableKonvaShapes/ModifiableBezier.tsx
@@ -2,20 +2,23 @@ import React, { FunctionComponent } from 'react';
 import { Line } from 'react-konva';
 import Coord from '../../types/Coord';
 import { ModifiableLinearShape } from '../../types/ModifiableShape';
-import PointModifier from '../../types/PointModifier';
 import {
   applyModifiers,
   getMultiplePoints,
 } from './modifiableKonvaShapes.service';
 
-export interface Bezier {
-  point: Coord;
-  b: Coord[];
-}
-
 interface Props extends ModifiableLinearShape {
   from: Coord;
-  to: Bezier[];
+  to: Coord[][];
+}
+
+interface BesierLine {
+  start: Coord;
+  points: number[];
+}
+
+function fixBezierOrder(endPoints: number[]): number[] {
+  return endPoints.length <= 4 ? [0, 0, ...endPoints] : endPoints;
 }
 
 const ModifiableBezier: FunctionComponent<Props> = ({
@@ -23,24 +26,80 @@ const ModifiableBezier: FunctionComponent<Props> = ({
   to,
   modifiers,
   color,
+  closed,
   ...rest
 }) => {
   const startPoint = modifiers ? applyModifiers(from, modifiers) : from;
 
-  // const modifiedEndPoints = modifiers
-  //   ? to.map((point) => applyModifiers(point, modifiers))
-  //   : to;
-  // const points = getMultiplePoints(startPoint, modifiedEndPoints);
+  const modifiedEndPoints: Coord[][] = modifiers
+    ? to.map((bezierEndPoint) =>
+        bezierEndPoint.map((point) => applyModifiers(point, modifiers))
+      )
+    : to;
+
+  const firstLine: BesierLine = {
+    start: startPoint,
+    points: [
+      0,
+      0,
+      ...fixBezierOrder(getMultiplePoints(startPoint, modifiedEndPoints[0])),
+    ],
+  };
+
+  const bezierLines: BesierLine[] = modifiedEndPoints.slice(1).reduce(
+    (acc, current) => {
+      const prevLine = acc[acc.length - 1];
+
+      const prevEndPoint: Coord = {
+        x: prevLine.points[prevLine.points.length - 2],
+        y: prevLine.points[prevLine.points.length - 1],
+      };
+
+      const line: BesierLine = {
+        start: prevEndPoint,
+        points: [
+          0,
+          0,
+          ...fixBezierOrder(getMultiplePoints(prevEndPoint, current)),
+        ],
+      };
+
+      return [...acc, line];
+    },
+    [firstLine]
+  );
+
+  if (closed) {
+    const prevLinePoints = modifiedEndPoints[modifiedEndPoints.length - 1];
+    const prevEndPoint = prevLinePoints[prevLinePoints.length - 1];
+
+    bezierLines.push({
+      start: prevEndPoint,
+      points: [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        ...getMultiplePoints(prevEndPoint, [startPoint]),
+      ],
+    });
+  }
 
   return (
-    <Line
-      x={startPoint.x}
-      y={startPoint.y}
-      points={[0, 0, []]}
-      stroke={color || 'black'}
-      {...rest}
-      bezier
-    />
+    <>
+      {bezierLines.map((line) => (
+        <Line
+          x={line.start.x}
+          y={line.start.y}
+          points={line.points}
+          stroke={color || 'black'}
+          bezier
+          {...rest}
+        />
+      ))}
+    </>
   );
 };
 
