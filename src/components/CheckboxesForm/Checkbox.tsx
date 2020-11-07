4bd488e7dae735d6e1321b05ffa3726aaa5edff0
vladhuk <vladhookovskiy@gmail.com>
2020-11-07 19:46:08 +0200

    - Add types to checkbox; - Fix position;

--- src/components/CheckboxesForm/Checkbox.tsx
+++ src/components/CheckboxesForm/Checkbox.tsx
@@ -1,18 +1,32 @@
 import React, { FunctionComponent } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
+export enum CheckboxType {
+  CHECKBOX = 'checkbox',
+  SWITCH = 'switch',
+}
+
 export interface FormCheckbox {
   title: string;
   value: boolean;
   setValue(value: boolean): void;
+  type?: CheckboxType;
 }
 
-const Input: FunctionComponent<FormCheckbox> = ({ title, value, setValue }) => (
+const Input: FunctionComponent<FormCheckbox> = ({
+  title,
+  value,
+  setValue,
+  type,
+}) => (
   <Form.Group as={Row} key={title}>
     <Form.Label column>{title}</Form.Label>
-    <Col md={3} className="d-flex align-items-center">
+    <Col md={3} className="d-flex align-items-center justify-content-center">
       <Form.Check
-        type="checkbox"
+        custom
+        id={title}
+        label=""
+        type={type}
         checked={value}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
           setValue(event.target.checked)
