162216f0a9468ed479cd07aa87b70423211b020f
vladhuk <vladhookovskiy@gmail.com>
2020-11-08 03:42:58 +0200

    - Fixing routing for gh pages...

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import { Nav, Navbar } from 'react-bootstrap';
 const Header: FunctionComponent = () => (
   <Navbar bg="light">
     <Nav>
-      <Nav.Link href={`/${process.env.PUBLIC_URL}/#/lab1`}>Lab 1</Nav.Link>
-      <Nav.Link href={`/${process.env.PUBLIC_URL}/#/lab2`}>Lab 2</Nav.Link>
-      <Nav.Link href={`/${process.env.PUBLIC_URL}/#/lab3`}>Lab 3</Nav.Link>
+      <Nav.Link href={`${process.env.PUBLIC_URL}/#/lab1`}>Lab 1</Nav.Link>
+      <Nav.Link href={`${process.env.PUBLIC_URL}/#/lab2`}>Lab 2</Nav.Link>
+      <Nav.Link href={`${process.env.PUBLIC_URL}/#/lab3`}>Lab 3</Nav.Link>
     </Nav>
   </Navbar>
 );
