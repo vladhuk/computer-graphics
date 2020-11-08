d5393eefe42fb6ceb1c9ad3cde446bc0bf9f8e23
vladhuk <vladhookovskiy@gmail.com>
2020-11-08 02:52:48 +0200

    - Add basename to router;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -399,7 +399,7 @@ const App: FunctionComponent = () => {
   return (
     <>
       <Header />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/lab1">
             {lab1}
